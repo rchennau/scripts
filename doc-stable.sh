57ec884fd0eb2bc517437a87c9bdae5a3cb1c9b3
rchennau <rchennau@gmail.com>
2023-05-30 17:11:18 +0000

    Check if block device exist and create XFS

--- doc-stable.sh
+++ doc-stable.sh
@@ -1,4 +1,32 @@
-sudo sudo mkfs -t xfs /dev/nvme1n1
+#!/bin/bash
+# set -x 								# Enable debug mode in bash
+echo on
+
+device=/dev/nvme1n1						# define the local variable block device
+mount_point=/home/ubuntu/app/data				# define the local variable mount point
+
+/home/ubuntu/scripts/update_route53.sh				# run the script to update route53 with current IP
+
+
+## Check if the device exists and is a block device
+if [ -b "$device" ]; then
+	echo "Device $device exists."
+	fs_type=$(blkid -o value -s TYPE "$device")
+	if [ "$fs_type" == "xfs" ]; then
+		echo "Device $device has an XFS file system"
+	else
+		echo "Device $device does not have an XFS file system"
+		echo "Trying to create XFS on $device ."
+		sudo mkfs -t xfs "$device"
+		if [ "$fs_type" == "xfs" ]; then
+			echo "Device $device XFS created."
+		else
+			echo "Unable to create XFS file sysetm on $device.  Exiting"
+			exit 1
+		fi
+	fi
+else
+
 sudo mount /dev/nvme1n1 /home/ubuntu/app/data
 sudo aws s3 cp s3://postwonder-models /home/ubuntu/app/data --recursive
 sudo chown -R ubuntu:ubuntu /home/ubuntu/app/data
