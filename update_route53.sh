5961febae1fca5af8cf7a1e9b3bd4abd20957012
Richard Chennault <rchennau@users.noreply.github.com>
2023-08-20 19:53:20 -0700

    I hate commas

--- update_route53.sh
+++ update_route53.sh
@@ -31,7 +31,7 @@ else
                     "TTL":300,
                     "AliasTarget": {
                         "ResourceRecords" : [{
-                            "DNSName": "'$cname_record_value'",
+                            "DNSName": "'$cname_record_value'"
                         }]
                     }
                 }
