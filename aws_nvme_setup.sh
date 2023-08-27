7bf363c03b6b45f54fe6e90f335c145bba5a5acf
Richard D. Chennault <rchennau@gmail.com>
2023-08-27 09:09:28 -0700

    minor errors

--- aws_nvme_setup.sh
+++ aws_nvme_setup.sh
@@ -9,40 +9,44 @@ fs_type=$(df --output=fstype "$device" | tail -n 1)
 echo $fs_type 
 if [ -z "$fs_type" ]; then
     echo "ERROR: no file system. Check returned $fs_type for device type $device"
-	echo "Trying to create XFS on $device ."
-	
-	# Create XFS file system	
-	if sudo mkfs -t xfs "$device"; then
+    echo "Trying to create XFS on $device ."
+    
+    # Create XFS file system    
+    if sudo mkfs -t xfs "$device"; then
         fs_type="xfs"
-		echo "File system $fs_type created on device:$device"
-	else 
-		echo "Unable to create XFS file sysetm on $device.  Exiting"
-		exit 1
+        echo "File system $fs_type created on device:$device"
+    else 
+        echo "Unable to create XFS file system on $device. Exiting"
+        exit 1
     fi
 
 else
-	echo "File system $fs_type found on device:$device"
+    echo "File system $fs_type found on device:$device"
 fi 
-    if [ "$fs_type" = "xfs" ]; then
-		if [ -z "$sd_mount" ]; then
-			echo "$fs_type on $device is not mounted.  Mounting"
-			if sudo mount $device $sd_mount; then 
-				sudo chown $id:$id $sd_mount
-				echo "Mounted $fs_type on $device is mounted at $sd_mount" 
-    		else
-				# add code to read for answer and continue
-				echo "$fs_type on $device is not XFS. Attempt to overwrite?"
-				# Create XFS file system	
-				if sudo mkfs -t xfs "$device"; then
-        			fs_type="xfs"
-					echo "File system $fs_type created on device:$device"
-					if sudo mount $device $sd_mount; then 
-						sudo chown $id:$id $sd_mount
-						echo "Mounted $fs_type on $device is mounted at $sd_mount" 
-					fi
-				else 
-					echo "Unable to create XFS file sysetm on $device.  Exiting"
-					exit 1
-fi
 
-echo "$fs_type on $device is present and mounted."
+if [ "$fs_type" = "xfs" ]; then
+    if [ -z "$sd_mount" ]; then
+        echo "$fs_type on $device is not mounted. Mounting"
+        if sudo mount $device $sd_mount; then 
+            sudo chown $id:$id $sd_mount
+            echo "Mounted $fs_type on $device is mounted at $sd_mount" 
+        else
+            # add code to read for answer and continue
+            echo "$fs_type on $device is not XFS. Attempt to overwrite?"
+            # Create XFS file system    
+            if sudo mkfs -t xfs "$device"; then
+                fs_type="xfs"
+                echo "File system $fs_type created on device:$device"
+                if sudo mount $device $sd_mount; then 
+                    sudo chown $id:$id $sd_mount
+                    echo "Mounted $fs_type on $device is mounted at $sd_mount" 
+                fi
+            else 
+                echo "Unable to create XFS file system on $device. Exiting"
+                exit 1
+            fi
+        fi
+    else
+        echo "$fs_type on $device is present and mounted."
+    fi
+fi
