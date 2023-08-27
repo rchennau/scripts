9c87063678fea94409333d913d068c6d29d5eca9
Richard D. Chennault <rchennau@gmail.com>
2023-08-26 21:08:25 -0700

    Added more checking

--- aws_nvme_setup.sh
+++ aws_nvme_setup.sh
@@ -2,31 +2,43 @@
 
 # set device name
 device=/dev/nvme1n1	
-mount==/workspace
+sd_mount=/workspace
+id=$(id -u)
 
 fs_type=$(df --output=fstype "$device" | tail -n 1)
 
 if [ -n "$fs_type" ]; then
     echo "ERROR: no file system. Check returned $fs_type for device type $device"
 	echo "Trying to create XFS on $device ."
-	sudo mkfs -t xfs "$device"
-	if [ -n "$fs_type" ]; then
-		echo "File system $fs_type create on device:$device"
+	
+	# Create XFS file system	
+	if sudo mkfs -t xfs "$device"; then
+        fs_type="xfs"
+		echo "File system $fs_type created on device:$device"
 	else 
 		echo "Unable to create XFS file sysetm on $device.  Exiting"
 		exit 1
     fi
-else
-    if [ "$fs_type" = "xfs" ]; then
-    	echo "File system $fs_type found on device:$device"
-		if [ -n "$mount" ]; then
-			echo "$fs_type on $device is not mounted.  Mounting"
-			sudo mount $device /workspace
-			sudo chown ubuntu:ubuntu /workspace
-		fi	
 
+else
+	echo "File system $fs_type found on device:$device"
+fi 
+    if [ "$fs_type" = "xfs" ]; then
+		if [ -n "$sd_mount" ]; then
+			echo "$fs_type on $device is not mounted.  Mounting"
+			if sudo mount $device $sd_mount; then 
+				sudo chown $id:$id $sd_mount
+				echo "Mounted $fs_type on $device is mounted at $sd_mount" 
+			else
+				echo "Unable to mount $fs_type on $device. Exiting"
+				exit 1
+			fi	
     else
-		echo "Expected file system type XFS.  Got $fs_type on $device.  Exiting"
-        exit 1
+		echo "Mount point not provided.  Skipping moutn process."
+	fi
+else
+	echo "Expected file system type XFS.  Got $fs_type on $device.  Exiting"
+    exit 1
 fi
+
 echo "$fs_type on $device is present and mounted."
---
+++ touch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aws_nvme_setup.sh
