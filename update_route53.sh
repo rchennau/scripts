cca94e47f3e0f391956f081d3cfb2f07f69baf86
rchennault-exelixis <rchennault@exelixis.com>
2023-08-29 11:07:32 -0700

    Make it complicated so we can support other domains

--- update_route53.sh
+++ update_route53.sh
@@ -2,11 +2,44 @@
 
 # Replace with your own values
 HOSTED_ZONE_ID="Z37WEDFVQ5POLT"
-RECORD_NAME="stable.chennault.net"
 TTL=60
-
+RECORD_NAME="stable.chennault.net"
 # Get the public IP address of the dynamic host
 PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)
+SILENT_MODE=""
+while getopts ":h:r:t:z:a:s" opt; do
+    case $opt in
+     h) echo "Usage: ${OPTARG}"
+     ;;
+     r) RECORD_NAME="$OPTARG"
+     ;;
+     t) TTL="$OPTARG"
+     ;;
+     z) HOSTED_ZONE_ID="$OPTARG"
+     ;;
+     a) PUBLIC_IP="$OPTARG"
+     ;;
+     s) SILENT_MODE="true"
+     ;;
+     *) echo "Unknown option $OPTARG"
+     esac
+done
+if [ -z $SILENT_MODE ]
+then
+    echo "Silent mode not set"
+    read -r -t 5 -p "Set domain [$RECORD_NAME]: " answer
+    if [ -z "$answer" ]; then
+        echo "RECORD_NAME: $answer not entered.  Using default: $RECORD_NAME"
+    fi
+    RECORD_NAME="$answer"
+else
+    echo "Silent mode set, using defaults"
+    if [ -z "$RECORD_NAME" ]
+    then
+        echo "Default DOMAIN is not set, exiting"
+        exit 1
+    fi
+fi
 
 # Update the A record with the new IP address
 aws route53 change-resource-record-sets \
@@ -22,4 +55,3 @@ aws route53 change-resource-record-sets \
             }
         }]
     }'
-
