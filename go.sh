e26f6d80355c378c28d6246ea5b4c853fcdcb5c0
Richard Chennault <rchennau@gmail.com>
2023-09-07 20:53:32 +0000

    Correcting file name

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-update_route53_config.sh
-aws_nvme_setup.sh
-stable-diffusion-setup.sh
-stable-diffusion-run.sh
+~/update_route53.sh
+~/aws_nvme_setup.sh
+~/stable-diffusion-setup.sh
+~/stable-diffusion-run.sh
