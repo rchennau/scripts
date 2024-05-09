8a85d964804d4b6ad746d392db1996a68ab6975a
rchennau <rchennau@gmail.com>
2024-05-09 19:11:09 +0000

    Changed based on current ssytem

--- link.sh
+++ link.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cd /workspace/stable-diffusion-webui
-ln -sf ~/scripts/styles.csv .
-ln -sf ~/scripts/webui-user.sh .
-mkdir /workspace/stable-diffusion-webui/outputs
+# cd /workspace/stable-diffusion-webui
+# ln -sf ~/scripts/styles.csv .
+# ln -sf ~/scripts/webui-user.sh .
+~/goofys postwonder-outputs ~/stable-diffusion-webui/outputs
+mkdir /workspace/models
+aws s3 cp s3://postwonder-models /workspace/models
 ~/goofys postwonder-outputs /workspace/stable-diffusion-webui/outputs
-sudo aws s3 cp s3://postwonder-extensions/ext.tar.gz /workspace/stable-diffusion-webui
-sudo tar xvf ext.tar.gz
+# sudo aws s3 cp s3://postwonder-extensions/ext.tar.gz /workspace/stable-diffusion-webui
+# sudo tar xvf ext.tar.gz
