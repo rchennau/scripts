c7179b9384ba2a45cf0f1614be7f8fb2937ec94c
rchennau <rchennau@gmail.com>
2024-05-09 19:10:40 +0000

    Adjusted based on current system

--- go.sh
+++ go.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ~/update_route53.sh
-~/aws_nvme_setup.sh
-~/stable-diffusion-setup.sh
-~/link.sh
 ~/swap.sh
+# ~/aws_nvme_setup.sh
+# ~/stable-diffusion-setup.sh
+~/link.sh
 ~/stable-diffusion-run.sh
