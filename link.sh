f20d51ec107b1bec5d9c039e8714aae93f877908
Ubuntu <ubuntu@ip-172-31-11-217.us-west-2.compute.internal>
2024-08-21 16:57:10 +0000

    Change the dirs

--- link.sh
+++ link.sh
@@ -3,7 +3,8 @@
 # cd /workspace/stable-diffusion-webui
 # ln -sf ~/scripts/styles.csv .
 # ln -sf ~/scripts/webui-user.sh .
-~/goofys postwonder-outputs ~/stable-diffusion-webui/outputs
+#~/goofys postwonder-outputs ~/stable-diffusion-webui/outputs
+rclone mount gdrive:outputs ~/stable-diffusion-webui-forge/outputs --vfs-cache-mode writes --progress --transfers=10 --checkers=8 --drive-chunk-size 64M --no-checksum --fast-list
 mkdir /workspace/models
 aws s3 cp s3://postwonder-models /workspace/models
 # sudo aws s3 cp s3://postwonder-extensions/ext.tar.gz /workspace/stable-diffusion-webui
