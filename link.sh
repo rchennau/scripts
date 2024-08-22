e0927887821fe7ff2c4792bec6ace1a4086aafeb
Ubuntu <ubuntu@ip-172-31-11-217.us-west-2.compute.internal>
2024-08-22 18:36:58 +0000

    add --daemon flag

--- link.sh
+++ link.sh
@@ -4,7 +4,7 @@
 # ln -sf ~/scripts/styles.csv .
 # ln -sf ~/scripts/webui-user.sh .
 #~/goofys postwonder-outputs ~/stable-diffusion-webui/outputs
-rclone mount gdrive:outputs ~/stable-diffusion-webui-forge/outputs --vfs-cache-mode writes --progress --transfers=10 --checkers=8 --drive-chunk-size 64M --no-checksum --fast-list
+rclone mount gdrive:outputs ~/stable-diffusion-webui-forge/outputs --vfs-cache-mode writes --progress --transfers=10 --checkers=8 --drive-chunk-size 64M --no-checksum --fast-list --daemon
 mkdir /workspace/models
 aws s3 cp s3://postwonder-models /workspace/models
 # sudo aws s3 cp s3://postwonder-extensions/ext.tar.gz /workspace/stable-diffusion-webui
