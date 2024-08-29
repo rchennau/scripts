50b8439c7e08ec0dac8a776b13527b26db69f122
Ubuntu <ubuntu@ip-172-31-15-248.us-west-2.compute.internal>
2024-08-29 22:48:40 +0000

    Minor change to be able to update output path to whatever.  I should turn this into a python script

--- link.sh
+++ link.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 models_folder="/workspace/models"
+output_path="/home/ubuntu/comfy/ComfyUI/output"
 
 # cd /workspace/stable-diffusion-webui
 # ln -sf ~/scripts/styles.csv .
 # ln -sf ~/scripts/webui-user.sh .
 #~/goofys postwonder-outputs ~/stable-diffusion-webui/outputs
-rclone mount gdrive:outputs ~/stable-diffusion-webui-forge/outputs --vfs-cache-mode writes --progress --transfers=10 --checkers=8 --drive-chunk-size 64M --no-checksum --fast-list --daemon
+# rclone mount gdrive:outputs ~/stable-diffusion-webui-forge/outputs --vfs-cache-mode writes --progress --transfers=10 --checkers=8 --drive-chunk-size 64M --no-checksum --fast-list --daemon
+rclone mount gdrive:outputs $output_path --vfs-cache-mode writes --progress --transfers=10 --checkers=8 --drive-chunk-size 64M --no-checksum --fast-list --daemon
 
 if [ ! -d "$models_folder" ]; then
 	echo "Folder '$models_folder' does not exist.  Creating new folder"
