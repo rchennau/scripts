15c628b945dc7e5fde081c1141b921c803fc858f
Richard Chennault <rchennau@users.noreply.github.com>
2024-08-29 23:09:13 -0700

    added actual parameter

--- aws_ec2_start.py
+++ aws_ec2_start.py
@@ -59,6 +59,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Start an EC2 instance with retries.")
     parser.add_argument("-i", "--instance_id", help="The ID of the EC2 instance to start")
     parser.add_argument("-r", "--max-retries", type=int, default=10, help="Maximum number of retries (default: 10)")
+    parser.add_argument("-p", "--stop", help="Stop the instance")
 
     args = parser.parse_args()
 
